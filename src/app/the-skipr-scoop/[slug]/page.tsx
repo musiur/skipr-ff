43e41a58f577dea6aa30d0d3b8908462b4be678a
musiur <musiur.opu@gmail.com>
2024-07-01 19:49:55 +0600

    skipr scoop page done

--- src/app/the-skipr-scoop/[slug]/page.tsx
+++ src/app/the-skipr-scoop/[slug]/page.tsx
@@ -5,6 +5,17 @@ import { Bebas_Neue } from "next/font/google";
 import Icon__Date from "../_utils/assets/date";
 import Icon__Eye from "../_utils/assets/eye";
 import Image from "next/image";
+import Icon__Twitter from "../_utils/assets/twitter";
+import Icon__FB from "../_utils/assets/fb";
+import Icon__Instagram from "../_utils/assets/instagram";
+import Icon__Youtube from "../_utils/assets/youtube";
+import Icon__Linkdin from "../_utils/assets/linkedin";
+import Icon__Tiktok from "../_utils/assets/tiktok";
+import Icon__WhatsApp from "../_utils/assets/whatsapp";
+import Icon__Redit from "../_utils/assets/redit";
+import Navbar from "@/components/layouts/navbar";
+import Icon__Back from "../_utils/assets/back";
+import Link from "next/link";
 
 const bebas_neue = Bebas_Neue({
   weight: "400",
@@ -15,87 +26,106 @@ const bebas_neue = Bebas_Neue({
 const Page = () => {
   return (
     <div>
-      <ANIM__FadeInOutOnScroll className="flex flex-col min-[600px]:flex-row items-start min-[600px]:items-center justify-between gap-4">
-        <ANIM__FadeInOutOnScroll className="space-y-4">
-          <h1
-            className={cn(
-              bebas_neue.className,
-              "text-left leading-10 uppercase 2xl:text-[60px]"
-            )}
-          >
-            THE POWER OF AUTONOMY
-          </h1>
-          <div className="flex flex-col min-[400px]:flex-row items-center justify-end min-[400px]:justify-between gap-4">
-            <div className="flex items-center gap-2">
-              <Icon__Date className="w-4 h-4" />
-              <span className="text-xs">Jun 15, 2024</span>
+      <Navbar color="black" />
+      <div className="container section pt-36">
+        <BackLink />
+
+        <ANIM__FadeInOutOnScroll className="w-full grid grid-cols-1 gap-4 py-16">
+          <ANIM__FadeInOutOnScroll className="space-y-4 w-full">
+            <h1
+              className={cn(
+                bebas_neue.className,
+                "text-left leading-10 uppercase 2xl:text-[60px]"
+              )}
+            >
+              THE POWER OF AUTONOMY
+            </h1>
+            <div className="w-full flex flex-col min-[600px]:flex-row items-start min-[600px]:items-center justify-between gap-4">
+              <div className="flex items-center gap-4">
+                <div className="flex items-center gap-2">
+                  <Icon__Date className="w-4 h-4" />
+                  <span className="text-xs">Jun 15, 2024</span>
+                </div>
+                <div className="flex items-center gap-2">
+                  <Icon__Eye className="w-4 h-4" />
+                  <span className="text-xs">1234</span>
+                </div>
+              </div>
+              <div className="flex items-center justify-end gap-2">
+                <Icon__FB className="fill-darkish" />
+                <Icon__Twitter className="fill-darkish" />
+                <Icon__Instagram className="fill-darkish" />
+                <Icon__Youtube className="fill-darkish" />
+                <Icon__Linkdin className="fill-darkish" />
+                <Icon__Tiktok className="fill-darkish" />
+                <Icon__WhatsApp className="fill-darkish" />
+                <Icon__Redit className="fill-darkish" />
+              </div>
             </div>
-            <div className="flex items-center gap-2">
-              <Icon__Eye className="w-4 h-4" />
-              <span className="text-xs">1234</span>
-            </div>
+          </ANIM__FadeInOutOnScroll>
+        </ANIM__FadeInOutOnScroll>
+
+        <ANIM__FadeInOutOnScroll className="space-y-10 py-10">
+          <div className="w-full h-64 relative">
+            <Image
+              src="/blog-hero.png"
+              alt="heroImage"
+              fill
+              className="object-cover object-center"
+            />
           </div>
+          <ANIM__FadeInOutOnScroll className="space-y-10">
+            <p>
+              Eiusmod excepteur duis fugiat nulla pariatur consectetur deserunt
+              laborum aliquip laborum ullamco mollit qui do. Magna laboris
+              pariatur duis et est esse laboris fugiat. Minim sit ea enim
+              exercitation pariatur esse eu magna irure proident. Tempor
+              voluptate elit reprehenderit pariatur culpa sint nulla nisi
+              fugiat. Fugiat duis in dolore do est. Nulla enim nulla dolore
+              aliqua tempor Lorem culpa minim qui sit dolor tempor.
+            </p>
+            <p>
+              Eiusmod excepteur duis fugiat nulla pariatur consectetur deserunt
+              laborum aliquip laborum ullamco mollit qui do. Magna laboris
+              pariatur duis et est esse laboris fugiat. Minim sit ea enim
+              exercitation pariatur esse eu magna irure proident. Tempor
+              voluptate elit reprehenderit pariatur culpa sint nulla nisi
+              fugiat. Fugiat duis in dolore do est. Nulla enim nulla dolore
+              aliqua tempor Lorem culpa minim qui sit dolor tempor.
+            </p>
+            <p>
+              Eiusmod excepteur duis fugiat nulla pariatur consectetur deserunt
+              laborum aliquip laborum ullamco mollit qui do. Magna laboris
+              pariatur duis et est esse laboris fugiat. Minim sit ea enim
+              exercitation pariatur esse eu magna irure proident. Tempor
+              voluptate elit reprehenderit pariatur culpa sint nulla nisi
+              fugiat. Fugiat duis in dolore do est. Nulla enim nulla dolore
+              aliqua tempor Lorem culpa minim qui sit dolor tempor.
+            </p>
+            <p>
+              Eiusmod excepteur duis fugiat nulla pariatur consectetur deserunt
+              laborum aliquip laborum ullamco mollit qui do. Magna laboris
+              pariatur duis et est esse laboris fugiat. Minim sit ea enim
+              exercitation pariatur esse eu magna irure proident. Tempor
+              voluptate elit reprehenderit pariatur culpa sint nulla nisi
+              fugiat. Fugiat duis in dolore do est. Nulla enim nulla dolore
+              aliqua tempor Lorem culpa minim qui sit dolor tempor.
+            </p>
+          </ANIM__FadeInOutOnScroll>
         </ANIM__FadeInOutOnScroll>
-        <div className="flex items-center justify-end gap-2">
-          <Icon__ChevronFill className="fill-darkish" />
-          <Icon__ChevronFill className="fill-darkish" />
-          <Icon__ChevronFill className="fill-darkish" />
-          <Icon__ChevronFill className="fill-darkish" />
-          <Icon__ChevronFill className="fill-darkish" />
-          <Icon__ChevronFill className="fill-darkish" />
-          <Icon__ChevronFill className="fill-darkish" />
-          <Icon__ChevronFill className="fill-darkish" />
-        </div>
-      </ANIM__FadeInOutOnScroll>
-      <ANIM__FadeInOutOnScroll className="space-y-10">
-        <Image
-          src="/"
-          alt="heroImage"
-          width={1000}
-          height={600}
-          className="w-full h-64"
-        />
-        <ANIM__FadeInOutOnScroll className="space-y-10">
-          <p>
-            Eiusmod excepteur duis fugiat nulla pariatur consectetur deserunt
-            laborum aliquip laborum ullamco mollit qui do. Magna laboris
-            pariatur duis et est esse laboris fugiat. Minim sit ea enim
-            exercitation pariatur esse eu magna irure proident. Tempor voluptate
-            elit reprehenderit pariatur culpa sint nulla nisi fugiat. Fugiat
-            duis in dolore do est. Nulla enim nulla dolore aliqua tempor Lorem
-            culpa minim qui sit dolor tempor.
-          </p>
-          <p>
-            Eiusmod excepteur duis fugiat nulla pariatur consectetur deserunt
-            laborum aliquip laborum ullamco mollit qui do. Magna laboris
-            pariatur duis et est esse laboris fugiat. Minim sit ea enim
-            exercitation pariatur esse eu magna irure proident. Tempor voluptate
-            elit reprehenderit pariatur culpa sint nulla nisi fugiat. Fugiat
-            duis in dolore do est. Nulla enim nulla dolore aliqua tempor Lorem
-            culpa minim qui sit dolor tempor.
-          </p>
-          <p>
-            Eiusmod excepteur duis fugiat nulla pariatur consectetur deserunt
-            laborum aliquip laborum ullamco mollit qui do. Magna laboris
-            pariatur duis et est esse laboris fugiat. Minim sit ea enim
-            exercitation pariatur esse eu magna irure proident. Tempor voluptate
-            elit reprehenderit pariatur culpa sint nulla nisi fugiat. Fugiat
-            duis in dolore do est. Nulla enim nulla dolore aliqua tempor Lorem
-            culpa minim qui sit dolor tempor.
-          </p>
-          <p>
-            Eiusmod excepteur duis fugiat nulla pariatur consectetur deserunt
-            laborum aliquip laborum ullamco mollit qui do. Magna laboris
-            pariatur duis et est esse laboris fugiat. Minim sit ea enim
-            exercitation pariatur esse eu magna irure proident. Tempor voluptate
-            elit reprehenderit pariatur culpa sint nulla nisi fugiat. Fugiat
-            duis in dolore do est. Nulla enim nulla dolore aliqua tempor Lorem
-            culpa minim qui sit dolor tempor.
-          </p>
-        </ANIM__FadeInOutOnScroll>
-      </ANIM__FadeInOutOnScroll>
+
+        <BackLink />
+      </div>
     </div>
   );
 };
 
 export default Page;
+
+const BackLink = () => {
+  return (
+    <Link href="/the-skipr-scoop">
+      <Icon__Back className="h-10 w-10" />
+    </Link>
+  );
+};
