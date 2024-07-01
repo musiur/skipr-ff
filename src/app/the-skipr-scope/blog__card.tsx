d9ac15c2f836da2ada9cdaa69012e53da479d0df
musiur <musiur.opu@gmail.com>
2024-07-01 07:14:12 +0600

    scoop page added

---
+++ src/app/the-skipr-scope/blog__card.tsx
@@ -0,0 +1,7 @@
+const BlogCard = () => {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-3 gap-4 p-4 rounded-lg">
+      {/* <Image src="" */}
+    </div>
+  );
+};
--- src/app/the-skipr-scope/page.tsx
+++ src/app/the-skipr-scope/page.tsx
@@ -1,15 +1,30 @@
+import ANIM__FadeInOutOnScroll from "@/components/anims/fadein.anim";
 import Navbar from "@/components/layouts/navbar";
+import { cn } from "@udecode/cn";
+import { Bebas_Neue } from "next/font/google";
+
+const bebas_neue = Bebas_Neue({
+  weight: "400",
+  style: "normal",
+  subsets: ["latin", "latin-ext"],
+});
 
 const Page = () => {
   return (
     <>
       <Navbar color="black" />
-      <p className="text-8xl font-bold container section">
-        Occaecat est dolor eiusmod ad culpa eiusmod in amet consequat quis.
-        Dolor nulla dolore et in quis laboris. Laboris eu do sunt anim ad minim
-        nostrud cillum ad. Officia sit nisi quis elit incididunt incididunt sunt
-        et minim dolore est.
-      </p>
+      <ANIM__FadeInOutOnScroll>
+        <h1
+          className={cn(
+            bebas_neue.className,
+            "text-center leading-10 uppercase 2xl:text-[60px]"
+          )}
+        >
+          The Skipr
+          <br />
+          Scoop
+        </h1>
+      </ANIM__FadeInOutOnScroll>
     </>
   );
 };
