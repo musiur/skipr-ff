f830710e113bbb5d19dbe9305ab35ca7092faeeb
musiur <musiur.opu@gmail.com>
2024-07-06 04:26:42 +0600

    cn removed from deploy__purple file

--- src/components/anims/deploy__purple.anim.tsx
+++ src/components/anims/deploy__purple.anim.tsx
@@ -3,14 +3,9 @@
 import React from "react";
 import Lottie from "lottie-react";
 import DeployPurpleJSON from "./deploy__purple.data.json";
-import { cn } from "@udecode/cn";
 
-const ANIM__DeployPurple = ({ className }: { className?: string }) => (
-  <Lottie
-    animationData={DeployPurpleJSON}
-    loop={true}
-    className={cn(className, "")}
-  />
+const ANIM__DeployPurple = () => (
+  <Lottie animationData={DeployPurpleJSON} loop={true} />
 );
 
 export default ANIM__DeployPurple;
