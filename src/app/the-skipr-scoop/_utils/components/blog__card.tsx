b76840dff22e36d8c2c7ae64e84da86c611ab4cf
musiur <musiur.opu@gmail.com>
2024-07-06 04:11:00 +0600

    responsive ness issue fixed in the skipr scoop

--- src/app/the-skipr-scoop/_utils/components/blog__card.tsx
+++ src/app/the-skipr-scoop/_utils/components/blog__card.tsx
@@ -5,8 +5,8 @@ import Link from "next/link";
 
 const BlogCard = () => {
   return (
-    <div className="grid grid-cols-1 [@media(min-width:860px)]:grid-cols-2 [@media(min-width:1150px)]:grid-cols-5 gap-10">
-      <div className="col-span-1 [@media(min-width:1150px)]:col-span-3 relative min-h-[200px]">
+    <div className="grid grid-cols-1 min-[860px]:grid-cols-2 min-[1150px]:grid-cols-5 gap-10">
+      <div className="col-span-1 min-[1150px]:col-span-3 relative min-h-[200px]">
         <Image
           src="/scoop1.png"
           alt="scoop1"
@@ -14,7 +14,7 @@ const BlogCard = () => {
           className="object-cover object-center"
         />
       </div>
-      <div className="col-span-1 [@media(min-width:1150px)]:col-span-2 space-y-4">
+      <div className="col-span-1 min-[1150px]:col-span-2 space-y-4">
         <h3 className="font-semibold">WHY PRIVACY MATTERS</h3>
         <p className="text-sm">
           Protectng your online privacy helps safeguard sensitve personal
@@ -22,7 +22,7 @@ const BlogCard = () => {
           passwords from falling into the wrong hands. This reduces the risk of
           identty thef, fraud, and other cybercrimes...
         </p>
-        <div className="flex flex-col [@media(min-width:400px)]:flex-row items-center justify-end [@media(min-width:400px)]:justify-between gap-4">
+        <div className="flex flex-col min-[400px]:flex-row items-center justify-end min-[400px]:justify-between gap-4">
           <div className="flex items-center gap-2">
             <div className="flex items-center gap-2">
               <Icon__Date className="w-4 h-4" />
