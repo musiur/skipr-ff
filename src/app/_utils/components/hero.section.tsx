c55e82849978d65d97d006ade3ee73d4a83ba684
musiur <musiur.opu@gmail.com>
2024-07-06 18:14:35 +0600

    home shield image changed

--- src/app/_utils/components/hero.section.tsx
+++ src/app/_utils/components/hero.section.tsx
@@ -83,15 +83,15 @@ const Section__HomeHero = () => {
             </li>
           </ul>
         </ANIM__FadeInOutOnScroll>
-        {/* <div className="mt-auto">
+        <div className="mt-auto">
           <Image
-            src="/heroshield.png"
+            src="/home-shield.png"
             alt="heroshield"
             width={1000}
             height={1000}
             className="min-w-[250px] max-w-[300px] h-full"
           />
-        </div> */}
+        </div>
       </div>
       <div className="absolute top-0 -right-[16%] w-[36vw] h-[100dvh] rounded-l-full z-[-1] bg-gradient-to-r from-[#10EDC490] via-[#0CF5C5] to-[#72ffe3] filter blur-[120px] animate-fadeInDown"></div>
     </section>
